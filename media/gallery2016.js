430d3e08574f7d5eed5df29f186d24ecadc7b0d6
Phill Conrad <pconrad@cs.ucsb.edu>
2016-12-30 18:08:47 -0800

    more images

--- media/gallery2016.js
+++ media/gallery2016.js
@@ -4,12 +4,16 @@ jQuery(document).ready( function(){
 	["2016SpringGroupPhoto.jpg", "Fratelli Spring 2016 Official Group Photo"],
 	["FRATELLIPostcardJune27.jpg", "Fratelli June 27 2016 Concert Postcard"],
 	["FratelliSingsOrlandoVigil.jpg","Fratelli sings in downtown Santa Barbara at the candlelight vigil for the victims of the June 2016 tragedy in Orlando."],
-	["FratelliHolidayConcertPoster.jpg", "Fratelli December 2016 Holiday Concert Poster"],
 	["2016_fall_group_photo.jpg","Fratelli Fall 2016 Official Group Photo"],
 	["ZackKTMSInterviewJimSirianni.jpg", "Zack's December 2016 Interview with KTMS's Jim Sirianni"],
 	["HeritageHouseSingoutNov28_2016.jpg", "Heritage House Singout, November 28, 2016"],
 	["HeritageHouseSingoutNov28_2016a.jpg", "Heritage House Singout, November 28, 2016"],
-	["HeritageHouseSingoutNov28_2916b.jpg", "Heritage House Singout, November 28, 2016"]	    
+	["HeritageHouseSingoutNov28_2916b.jpg", "Heritage House Singout, November 28, 2016"],
+	["FratelliHolidayConcertPoster.jpg", "Fratelli December 2016 Holiday Concert Poster"],
+	["HolidayConcert1.jpg","Fratelli December 2016 Holiday Concert"],
+	["HolidayConcert2.jpg","Fratelli December 2016 Holiday Concert"],
+	["HolidayConcert3.jpg","Fratelli December 2016 Holiday Concert"],
+	
     ];
     
     
